f2e095f20d9168aadead7e92b1ee33b4d37b8973
Jeonghun HAN <jasmin@o.shinhan.ac.kr>
2023-09-21 11:06:31 +0900

    nuxt 공부중

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>spa 방식임</h1>
+    <p>이거 spa 방식으로 돌아감</p>
+
+    <!--오늘 날짜-->
+    <p>오늘 {{Today}}임...</p>
+  </div>
+</template>
+
+<script>
+//import moment
+
+import moment from 'moment';
+
+
+export default {
+  name:  'Index'
+
+  //data.return
+  data() {
+    return{
+      today: moment().format('DD-MM-YYYY'),
+    };
+  },
+};
+</script>
